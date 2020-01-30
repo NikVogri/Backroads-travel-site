e7da75d2e1b3b1691c4a36d85d2a4127ae28a2a3
Nik Vogrinec <vogrinec.nik@gmail.com>
2020-01-30 22:05:46 +0100

    added single tour query

--- src/template/tour-template.component.jsx
+++ src/template/tour-template.component.jsx
@@ -1,7 +1,46 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-const Tour = () => {
-  return <div>ello template here</div>
+const Tour = ({ data }) => {
+  const {
+    name,
+    price,
+    country,
+    days,
+    description: { description },
+    images,
+    start,
+    journey,
+  } = data.tour
+  const [mainImage, ...tourImages] = images
+  console.log(tourImages)
+  return (
+    <div>
+      <h1>{data.tour.name}</h1>
+    </div>
+  )
 }
-
+export const query = graphql`
+  query($slug: String!) {
+    tour: contentfulTour(slug: { eq: $slug }) {
+      price
+      name
+      country
+      days
+      start(formatString: "dddd MMM Do, YYYY")
+      description {
+        description
+      }
+      journey {
+        day
+        info
+      }
+      images {
+        fluid {
+          src
+        }
+      }
+    }
+  }
+`
 export default Tour
