9791fbee86e6f5a0c703579d1c4088997bd44530
Nik Vogrinec <vogrinec.nik@gmail.com>
2020-01-31 21:41:12 +0100

    added full tours page

--- src/template/tour-template.component.jsx
+++ src/template/tour-template.component.jsx
@@ -1,5 +1,12 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Layout from "../components/Layout/Layout.component"
+import StyledHero from "../components/StyledHero/StyledHero.component"
+import styles from "./template.module.css"
+import Img from "gatsby-image"
+import { FaMoneyBillWave, FaMap } from "react-icons/fa"
+import Day from "../components/Day/Day.component"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 const Tour = ({ data }) => {
   const {
@@ -13,11 +20,47 @@ const Tour = ({ data }) => {
     journey,
   } = data.tour
   const [mainImage, ...tourImages] = images
-  console.log(tourImages)
   return (
-    <div>
-      <h1>{data.tour.name}</h1>
-    </div>
+    <Layout>
+      <StyledHero img={mainImage.fluid} />
+      <section className={styles.template}>
+        <div className={styles.center}>
+          <div className={styles.images}>
+            {tourImages.map((image, index) => (
+              <Img
+                fluid={image.fluid}
+                key={index}
+                alt="single tour"
+                className={styles.image}
+              />
+            ))}
+          </div>
+          <h2>{name}</h2>
+          <div className={styles.info}>
+            <p>
+              <FaMoneyBillWave className={styles.icon} />
+              starting from ${price}
+            </p>
+            <p>
+              <FaMap className={styles.icon} />
+              {country}
+            </p>
+          </div>
+          <h4>starts on : {start}</h4>
+          <h4>duration : {days} days</h4>
+          <p className={styles.desc}>{description}</p>
+          <h2>daily schedule</h2>
+          <div className={styles.journey}>
+            {journey.map((item, index) => (
+              <Day day={item.day} info={item.info} key={index} />
+            ))}
+          </div>
+          <AniLink fade to="/tours" className="btn-primary">
+            Back to tours
+          </AniLink>
+        </div>
+      </section>
+    </Layout>
   )
 }
 export const query = graphql`
@@ -37,7 +80,7 @@ export const query = graphql`
       }
       images {
         fluid {
-          src
+          ...GatsbyContentfulFluid_tracedSVG
         }
       }
     }
